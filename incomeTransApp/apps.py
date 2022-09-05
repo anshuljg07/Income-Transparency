5d64ef066225c12541045acfdd7b1ff22721db57
Rabi Alaya <rabialaya@gmail.com>
2022-09-05 17:47:26 -0500

    finalized final project structure

--- incomeTransApp/apps.py
+++ incomeTransApp/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class IncometransparencyConfig(AppConfig):
+class IncometransappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'IncomeTransparency'
+    name = 'incomeTransApp'
--- incomeTransparency/urls.py
+++ incomeTransparency/urls.py
@@ -1,4 +1,4 @@
-"""IncomeApp URL Configuration
+"""incomeTransparency URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -14,8 +14,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
+    path('incomeTransApp/', include('incomeTransApp.urls')),
 ]
